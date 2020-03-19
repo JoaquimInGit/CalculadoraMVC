e8e0faf621cde09e9420851c083b36992d9ef9cd
Joaquim Tomas <joaquimemtomas@gmail.com>
2020-03-19 11:26:41 +0000

    commits

---
+++ CalculadoraMVC/CalculadoraMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CalculadoraMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
